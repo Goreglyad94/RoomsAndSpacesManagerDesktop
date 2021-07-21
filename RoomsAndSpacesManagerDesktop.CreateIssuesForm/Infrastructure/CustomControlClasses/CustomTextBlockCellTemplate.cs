8175bf279f52169d815365027ad74ca6967ab2ea
goreglyad94 <yaroslav.goreglyad123@gmail.com>
2021-07-21 11:36:21 +0300

    Создан проект "Создание заданий на помещения"

---
+++ RoomsAndSpacesManagerDesktop.CreateIssuesForm/Infrastructure/CustomControlClasses/CustomTextBlockCellTemplate.cs
@@ -0,0 +1,100 @@
+﻿using RoomsAndSpacesManagerDataBase.Dto.RoomInfrastructure;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace RoomsAndSpacesManagerDesktop.CreateIssuesForm.Infrastructure.CustomControlClasses
+{
+    class CustomTextBlockCellTemplate : TextBlock
+    {
+        public string PropertyName
+        {
+            get
+            {
+                return (string)GetValue(PropertyNameProperty);
+            }
+            set
+            {
+                SetValue(PropertyNameProperty, value);
+            }
+        }
+
+        public CustomTextBlockCellTemplate()
+        {
+            TextWrapping = TextWrapping.Wrap;
+        }
+
+        public static readonly DependencyProperty PropertyNameProperty =
+           DependencyProperty.Register("PropertyName", typeof(string), typeof(CustomTextBlockCellTemplate),
+            new FrameworkPropertyMetadata("", new PropertyChangedCallback(PropertyNameChanged)));
+
+        private static void PropertyNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((CustomTextBlockCellTemplate)d).PropertyName = ((CustomTextBlockCellTemplate)d).PropertyName;
+        }
+
+        public string FirstValue
+        {
+            get
+            {
+                return (string)GetValue(FirstValueProperty);
+            }
+            set
+            {
+                SetValue(FirstValueProperty, value);
+
+
+                string propValue = SecondValue?.GetType().GetProperty(PropertyName)?.GetValue(SecondValue)?.ToString();
+                if (FirstValue != propValue)
+                {
+                    Background = Brushes.Pink;
+
+                }
+                else
+                {
+                    Background = Brushes.Transparent;
+                }
+            }
+        }
+
+        public static readonly DependencyProperty FirstValueProperty =
+           DependencyProperty.Register("FirstValue", typeof(string), typeof(CustomTextBlockCellTemplate),
+            new FrameworkPropertyMetadata("", new PropertyChangedCallback(FirstValueChanged)));
+
+        private static void FirstValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((CustomTextBlockCellTemplate)d).FirstValue = ((CustomTextBlockCellTemplate)d).FirstValue;
+        }
+
+        public RoomNameDto SecondValue
+        {
+            get
+            {
+                return (RoomNameDto)GetValue(SecondValueProperty);
+            }
+            set
+            {
+                SetValue(SecondValueProperty, value);
+                string propValue = SecondValue?.GetType().GetProperty(PropertyName)?.GetValue(SecondValue)?.ToString();
+                if (FirstValue != propValue)
+                {
+                    Background = Brushes.Pink;
+                }
+                else
+                {
+                    Background = Brushes.Transparent;
+                }
+            }
+        }
+
+        public static readonly DependencyProperty SecondValueProperty =
+           DependencyProperty.Register("SecondValue", typeof(RoomNameDto), typeof(CustomTextBlockCellTemplate),
+            new FrameworkPropertyMetadata(null, new PropertyChangedCallback(SecondValueChanged)));
+
+        private static void SecondValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((CustomTextBlockCellTemplate)d).SecondValue = ((CustomTextBlockCellTemplate)d).SecondValue;
+        }
+
+    }
+}
---
+++ RoomsAndSpacesManagerDesktop.CreateIssuesForm/ViewModels/CreateIssuesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using RoomsAndSpacesManagerDesktop.ViewModelBase.ViewModelBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomsAndSpacesManagerDesktop.CreateIssuesForm.ViewModels
+{
+    class CreateIssuesViewModel : ViewModel
+    {
+
+    }
+}
